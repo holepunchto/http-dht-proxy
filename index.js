1246b1970d5f3648073cc2e7e9758c10b92578a5
Marco <maidh91@gmail.com>
2025-06-04 00:47:33 +1000

    remove try catch

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ const PORT = process.argv[2] || 8080
 const dht = new DHT()
 net.createServer((sock) => {
   proxy(sock, (host) => {
-    try {
-      const id = host.split('.')[0]
-      return dht.connect(idEnc.decode(id))
-    } catch (err) {
-      console.error('Proxy error:', err)
-      sock.destroy()
-    }
+    const id = host.split('.')[0]
+    return dht.connect(idEnc.decode(id))
   })
 }).listen(PORT, () => console.log(`HTTP-to-DHT proxy on ${PORT}`))
