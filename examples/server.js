1867ca5ce8ba5f1eca817482c61c364798a2d299
Marco <maidh91@gmail.com>
2025-06-04 00:20:32 +1000

    lint

--- examples/server.js
+++ examples/server.js
@@ -33,4 +33,3 @@ const server = dht.createServer((conn) => {
 const serverKeyPair = DHT.keyPair(Buffer.alloc(32).fill(NAME))
 server.listen(serverKeyPair)
 console.log('DHT public key', idEnc.normalize(serverKeyPair.publicKey))
-
