17e210636ed6c1c7319dde65b3e21d8bca97d303
Marco <maidh91@gmail.com>
2025-06-03 23:08:24 +1000

    use hypercore-id-encoding

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const net = require('net')
 const DHT = require('hyperdht')
 const proxy = require('http-forward-host')
+const idEnc = require('hypercore-id-encoding')
 
 const PORT = process.argv[2] || 8080
 
 const dht = new DHT()
 net.createServer((sock) => {
-  proxy(sock, (proxyTo) => dht.connect(Buffer.from(proxyTo, 'hex')))
+  proxy(sock, (proxyTo) => dht.connect(idEnc.decode(proxyTo)))
 }).listen(PORT, () => console.log(`HTTP-to-DHT proxy on ${PORT}`))
