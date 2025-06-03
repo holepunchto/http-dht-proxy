0b26e73b07c20886f5a0bb2f19773c639e88f622
Marco <maidh91@gmail.com>
2025-06-04 00:45:34 +1000

    fix requestUrl

--- examples/client.js
+++ examples/client.js
@@ -1,8 +1,8 @@
+const requestUrl = 'http://f7tnaio84i4r9n7escd7dp7ke3umumorm4yijfqupqwzkd3m533o.example.com'
 const proxyUrl = 'http://localhost:8080'
-const publicKey = 'f7tnaio84i4r9n7escd7dp7ke3umumorm4yijfqupqwzkd3m533o'
 
 fetch(proxyUrl, {
-  headers: { 'x-forwarded-for': publicKey },
+  headers: { 'x-forwarded-for': requestUrl },
   method: 'POST',
   body: JSON.stringify({ message: 'Hello from client!' })
 }).then(res => res.text())
