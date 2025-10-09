0605fbde9c061fd484585edd9deee47c9aa688c9
Marco <maidh91@gmail.com>
2025-10-09 19:04:54 +1100

    get server.address().port

--- bin.js
+++ bin.js
@@ -9,8 +9,7 @@ const proxy = require('.')
 const PORT = process.argv[2] || 8080
 
 const dht = new HyperDHT()
-net
-  .createServer((sock) => {
-    proxy(sock, (key) => dht.connect(idEnc.decode(key)))
-  })
-  .listen(PORT, () => console.log(`HTTP-to-DHT proxy on ${PORT}`))
+const server = net.createServer((sock) => {
+  proxy(sock, (key) => dht.connect(idEnc.decode(key)))
+})
+server.listen(PORT, () => console.log(`HTTP-to-DHT proxy on ${server.address().port}`))
